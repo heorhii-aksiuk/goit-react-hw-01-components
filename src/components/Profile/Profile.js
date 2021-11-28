62eae425ccf515c1bb96beed4d63d05ca4864a01
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-11-28 16:15:19 +0200

    Profile component done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,36 +1,33 @@
 import PropTypes from 'prop-types';
-import defaultAvatar from './defaultAvatar.png';
+import defaultAvatar from './Profile.jpg';
+import s from './Profile.module.css';
 
-function Profile({
-  img = defaultAvatar,
-  username,
-  tag,
-  location,
-  followers,
-  views,
-  likes,
-}) {
+function Profile({ img, username, tag, location, followers, views, likes }) {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={img ?? defaultAvatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img
+          src={img ?? defaultAvatar}
+          alt="User avatar"
+          className={s.avatar}
+        />
+        <p className={s.name}>{username}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
+      <ul className={s.stats}>
+        <li className={s.list}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
         </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
+        <li className={s.list}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
+        <li className={s.list}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
